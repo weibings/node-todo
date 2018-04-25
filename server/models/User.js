039e7695f3905274db12ade708738824573d09e7
bin wei <binwei1988@gmail.com>
2018-04-25 10:42:44 -0400

    added token

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,52 @@
 const {mongoose} = require('./../db/mongoose.js');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
-		minlength: 6
+		minlength: 1,
+		trim: true,
+		unique: true,
+		validate: validator.isEmail,
+		message: '{VALUE} is not a valid email'
 	},
 	password: {
 		type: String,
 		required:true,
 		minlength: 6
-	}
-});
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
+})
+
+UserSchema.methods.toJSON = function() {
+	let user = this;
+	return _.pick(user.toObject(), ['email', '_id']);
+}
+UserSchema.methods.generateAuthToken = function() {
+
+	let user = this;
+	let access = 'auth';
+	let token = jwt.sign({_id: user._id, access}, 'miawei').toString();
+	user.tokens.push({access, token});
+	console.log(user);
+	return user.save().then(() => {
+		return token;
+	});
+};
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
 
--- server/server.js
+++ server/server.js
@@ -90,6 +90,19 @@ app.patch('/todos/:id', (req, res) => {
 	})
 });
 
+app.post('/users', (req, res) => {
+	let body = _.pick(req.body, ['email', 'password']);
+	let user = new User(body);
+	console.log(user);
+	user.save().then(() => {
+
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+})
 app.listen(port, () => {
 	console.log(`Start port ${port}`);
 })
